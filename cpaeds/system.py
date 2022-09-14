7a8195969b17f6093fb03778ce0738e8e07e054f
braunsfeld <benedict.braunsfeld@boku.ac.at>
2022-09-14 14:44:14 +0200

    making checks none private class

--- cpaeds/system.py
+++ cpaeds/system.py
@@ -26,7 +26,7 @@ class SetupSystem(object):
         self.pertubation_file: str = self.config['system']['pert_file']
         self.sys_dir: str = self.config['system']['system_dir']
 
-    def _check_input_settings(self):
+    def check_input_settings(self):
         """
         A function that checks for the existence of the input files for a simulation.
         ----------
@@ -60,14 +60,14 @@ class SetupSystem(object):
             logger.critical(f"No pertubation file (.ptp) argument.")
             sys.exit(f"Error changing to output folder directory.")
 
-    def _check_system_dir(self):
+    def check_system_dir(self):
         """Checking if current dir is simulation dir"""
         if self.sys_dir == os.path.dirname(os.path.abspath(__file__)):
             pass
         else:
             os.chdir(self.sys_dir)
 
-    def _check_dirs(self):
+    def check_dirs(self):
         """Checking if path to input dir exist"""
         self.dir_list = get_dir_list()
         if os.path.basename(os.path.normpath(self.topo_dir)) in self.dir_list:
@@ -89,7 +89,7 @@ class SetupSystem(object):
             self.aeds_dir = f"{self.sys_dir}/aeds"
             logger.info(f"aeds folder created.")
 
-    def _check_input_files(self):
+    def check_input_files(self):
         """Checking if the input files .topo, .ptp, .cnf & .imd exist"""
         files = [f for f in os.listdir(self.topo_dir) if os.path.isfile(os.path.join(self.topo_dir, f))]
         if  self.topology_file in files:
@@ -116,7 +116,7 @@ class SetupSystem(object):
         self.ref_imd = f"{imd_list[-1]}" 
         logger.info(f"{self.ref_imd} found.")
 
-    def _check_simulation_settings(self):
+    def check_simulation_settings(self):
         """Checks if the set of input parameter in the settings.yaml is complete"""
         if self.config['simulation']['NSTATS']:
             if int(self.config['simulation']['NSTATS']) > 1:
