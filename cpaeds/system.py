1807dbea9edb6ed5a9f12159cb6fb9b17dab6fdc
braunsfeld <benedict.braunsfeld@boku.ac.at>
2022-09-14 14:38:21 +0200

    completing checks in system

--- cpaeds/system.py
+++ cpaeds/system.py
@@ -19,13 +19,14 @@ class SetupSystem(object):
         settings: dict
             the configuration dictionary obtained with utils.load_config_yaml
         """
-        self.md_dir: str = settings['system']['md_dir']
-        self.output_dir: str = settings['system']['output_dir_name']
-        self.topology_file: str = settings['system']['topo_file']
-        self.pertubation_file: str = settings['system']['pert_file']
-        self.sys_dir: str = settings['system']['system_dir']
+        self.config: dict = settings
+        self.md_dir: str = self.config['system']['md_dir']
+        self.output_dir: str = self.config['system']['output_dir_name']
+        self.topology_file: str = self.config['system']['topo_file']
+        self.pertubation_file: str = self.config['system']['pert_file']
+        self.sys_dir: str = self.config['system']['system_dir']
 
-    def check_input_settings(self):
+    def _check_input_settings(self):
         """
         A function that checks for the existence of the input files for a simulation.
         ----------
@@ -50,59 +51,57 @@ class SetupSystem(object):
             self.topology_file: str = os.path.basename(os.path.normpath(self.topology_file))
             logger.info(f"Topo file set to {self.topology_file}.")
         else:
-            logger.critical("Missing topo file argument.")
-            sys.exit("Error changing to output folder directory.")
+            logger.critical(f"Missing topo file argument.")
+            sys.exit(f"Error changing to output folder directory.")
         if self.pertubation_file:
             self.pertubation_file: str = os.path.basename(os.path.normpath(self.pertubation_file))
             logger.info(f"Pertubation file set to {self.pertubation_file}.")
         else:
-            logger.critical("No pertubation file (.ptp) argument.")
-            sys.exit("Error changing to output folder directory.")
+            logger.critical(f"No pertubation file (.ptp) argument.")
+            sys.exit(f"Error changing to output folder directory.")
 
-    def check_system_dir(self):
+    def _check_system_dir(self):
         """Checking if current dir is simulation dir"""
         if self.sys_dir == os.path.dirname(os.path.abspath(__file__)):
             pass
         else:
             os.chdir(self.sys_dir)
 
-    def check_dirs(self,settings_loaded):
+    def _check_dirs(self):
         """Checking if path to input dir exist"""
         self.dir_list = get_dir_list()
         if os.path.basename(os.path.normpath(self.topo_dir)) in self.dir_list:
             logger.info(f"Topo folder found.")
         else:
             logger.critical(f"Missing topo folder.")
-
-
-
-        if os.path.basename(os.path.normpath(settings_loaded['system']['md_dir'])) in self.dir_list:
-            print("MD folder found.")
+        if os.path.basename(os.path.normpath(self.md_dir)) in self.dir_list:
+            logger.info(f"MD folder found.")
         else:
-            print("Missing MD folder.")
+            logger.critical(f"Missing MD folder.")
         if 'aeds' in self.dir_list:
-            settings_loaded['system']['aeds_dir'] = f"{settings_loaded['system']['system_dir']}/aeds"
-            print("aeds folder already exists.")
+            self.aeds_dir = f"{self.sys_dir}/aeds"
+            logger.info(f"aeds folder already exists.")
         else:
             try:
                 os.mkdir('aeds')
             except FileExistsError:
                 pass
-            settings_loaded['system']['aeds_dir'] = f"{settings_loaded['system']['system_dir']}/aeds"
-            print("AEDS folder created.")
+            self.aeds_dir = f"{self.sys_dir}/aeds"
+            logger.info(f"aeds folder created.")
 
-    def check_input_files(settings_loaded):
-        files = [f for f in os.listdir(settings_loaded['system']['topo_dir']) if os.path.isfile(os.path.join(settings_loaded['system']['topo_dir'], f))]
-        if  settings_loaded['system']['topo_file'] in files:
-            print(f"{settings_loaded['system']['name']}.top found.")
+    def _check_input_files(self):
+        """Checking if the input files .topo, .ptp, .cnf & .imd exist"""
+        files = [f for f in os.listdir(self.topo_dir) if os.path.isfile(os.path.join(self.topo_dir, f))]
+        if  self.topology_file in files:
+            logger.info(f"Topology file found.")
         else:
-            print("No topo file exists.")
-        if settings_loaded['system']['pert_file'] in files:
-            print(f"{settings_loaded['system']['pert_file']} found.")
+            logger.critical(f"No topology file exists.")
+        if self.pertubation_file in files:
+            logger.info(f"Pertubation file found.")
         else:
-            print("No ptp file exists.")
+            logger.critical(f"No pertubation file exists.")
 
-        files = [f for f in os.listdir(settings_loaded['system']['md_dir']) if os.path.isfile(os.path.join(settings_loaded['system']['md_dir'], f))]
+        files = [f for f in os.listdir(self.md_dir) if os.path.isfile(os.path.join(self.md_dir, f))]
         cnf_list=[]
         imd_list=[]
         for file in files:
@@ -112,31 +111,36 @@ class SetupSystem(object):
                 imd_list.append(file)
         cnf_list.sort(key=natural_keys)
         imd_list.sort(key=natural_keys)
-        settings_loaded['system']['cnf_file'] = f"{cnf_list[-1]}"
-        print(f"{settings_loaded['system']['cnf_file']} found.")
-        settings_loaded['system']['ref_imd'] = f"{imd_list[-1]}" 
-        print(f"{settings_loaded['system']['ref_imd']} found.")
+        self.cnf_file = f"{cnf_list[-1]}"
+        logger.info(f"{self.cnf_file} found.")
+        self.ref_imd = f"{imd_list[-1]}" 
+        logger.info(f"{self.ref_imd} found.")
 
-    def check_simulation_settings(settings_loaded):
-        nstats_exists = False
-        if settings_loaded['simulation']['NSTATS']:
-            nstats_exists = True
-            if nstats_exists == True and int(settings_loaded['simulation']['NSTATS']) > 1:
-                print(f"Statistic mode with {settings_loaded['simulation']['NSTATS']} repetitions")  
-            else:
-                settings_loaded['simulation']['NSTATS'] = 1
-                print(f"Single run mode")
+    def _check_simulation_settings(self):
+        """Checks if the set of input parameter in the settings.yaml is complete"""
+        if self.config['simulation']['NSTATS']:
+            if int(self.config['simulation']['NSTATS']) > 1:
+                logger.info(f"Statistic mode with {self.config['simulation']['NSTATS']} repetitions")  
+        else:
+            self.config['simulation']['NSTATS'] = 1
+            logger.info(f"Single run mode")
+        if self.config['simulation']['NSTATES']:
+            if int(self.config['simulation']['NSTATES']) > 2:
+                logger.info(f"Multi-endstate run with {self.config['simulation']['NSTATES']} endstates")  
+        else:
+            self.config['simulation']['NSTATES'] = 2
+            logger.info(f"Default run with 2 endstates")
         if  ( 
-            settings_loaded['simulation']['parameters'].get('NRUN') == None or
-            settings_loaded['simulation']['parameters'].get('NSTLIM') == None or 
-            settings_loaded['simulation']['parameters'].get('NTPR') == None or
-            settings_loaded['simulation']['parameters'].get('NTWX') == None or
-            settings_loaded['simulation']['parameters'].get('NTWE') == None or
-            settings_loaded['simulation']['parameters'].get('dt') == None or 
-            settings_loaded['simulation']['parameters'].get('EMIN') == None or
-            settings_loaded['simulation']['parameters'].get('EMAX') == None or
-            settings_loaded['simulation']['parameters'].get('EIR_start') == None or
-            settings_loaded['simulation']['parameters'].get('EIR_range') == None or
-            settings_loaded['simulation']['parameters'].get('EIR_step_size') == None
+            self.config['simulation']['parameters'].get('NRUN') == None or
+            self.config['simulation']['parameters'].get('NSTLIM') == None or 
+            self.config['simulation']['parameters'].get('NTPR') == None or
+            self.config['simulation']['parameters'].get('NTWX') == None or
+            self.config['simulation']['parameters'].get('NTWE') == None or
+            self.config['simulation']['parameters'].get('dt') == None or 
+            self.config['simulation']['parameters'].get('EMIN') == None or
+            self.config['simulation']['parameters'].get('EMAX') == None or
+            self.config['simulation']['parameters'].get('EIR_start') == None or
+            self.config['simulation']['parameters'].get('EIR_range') == None or
+            self.config['simulation']['parameters'].get('EIR_step_size') == None
         ):
             raise KeyError("Parameterset is not complete")
